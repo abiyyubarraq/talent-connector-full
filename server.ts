1c83cc06a2887cc26b1eb9503e585d4ddaf535ad
Abiyyu Barraq <57933315+perset4n@users.noreply.github.com>
2022-09-13 15:49:17 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -51,10 +51,11 @@ const StartServer = () => {
   app.use("/api/profile", profileRoutes)
   app.use("/api/post", postRoutes)
   
-  if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build')
-    app.get('*',(req,res)=>{
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     })
   }
 
