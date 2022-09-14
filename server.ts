b1132779c2ad3b7fbc897a396e3e44c2734bb29b
perset4n <siblal001@gmail.com>
2022-09-14 14:11:02 +0700

    deployment v1

--- server.ts
+++ server.ts
@@ -51,6 +51,13 @@ const StartServer = () => {
   app.use("/api/profile", profileRoutes)
   app.use("/api/post", postRoutes)
 
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"))
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+  }
+
   /** Error handling */
   app.use((req, res, next) => {
     const error = new Error("Not found")
